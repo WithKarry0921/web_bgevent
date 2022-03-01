93117e41fe22afcd31b41b0f7cf747167bbd0d13
wangjunke <2967512052@qq.com>
2022-03-01 16:59:39 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,15 @@ $.ajaxPrefilter(function(options) {
     // console.log(options.url);
     //在发起请求之前，拼接根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+        options.complete = function(res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    //获取用户信息
+    getUserInfo()
+
+    var layer = layui.layer
+        //给退出按钮添加点击事件
+    $('#btnLogout').on('click', function() {
+        // console.log('ok')
+
+        layer.confirm('确定退出登录', { icon: 3, title: '提示' }, function(index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+                //关闭confirm询问框
+            layer.close(index)
+        })
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        //headers就是请求头对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) return layui.layer.msg('获取用户信息失败！')
+                //调用渲染头像函数
+            renderAvatar(res.data)
+        },
+        //不管成功失败都会调用commplete函数
+        //complete函数配置到baseAPI.js中
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;' + name)
+    if (user.user_pic) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    }
+    $('.layui-nav-img').hide()
+    var first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+}
